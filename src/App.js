652d1ab539b143f716bd892d818a4a57097a318b
Flávio Bianchetti <fjb.developer@gmail.com>
2021-10-08 07:44:55 -0300

    Atendido exercicio 1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,36 @@ import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      pokemonIndex: 0,
+      pokemonLength: pokemons.length,
+    };
+  }
+
+  handleClick() {
+    if (this.state.pokemonIndex < this.state.pokemonLength - 1) {
+      this.setState((previusState, _props) => ({
+        pokemonIndex: previusState.pokemonIndex + 1
+      }));
+    } else {
+      this.setState({
+        pokemonIndex: 0
+      });
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemon={pokemons[this.state.pokemonIndex]} />
+        <button onClick={this.handleClick}>Next</button>
+      </div>
+    );
+  }
 }
 
 export default App;
